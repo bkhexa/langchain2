74c70f2fee581d6c062f92f0c03a67203e487a44
bkhexa <134046547+bkhexa@users.noreply.github.com>
2023-08-25 16:40:20 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,115 @@
+import streamlit as st
+import pandas as pd
+import json
+from tempfile import NamedTemporaryFile
+from agent import query_agent, create_agent
+import json.decoder
+
+def decode_response(response: str) -> dict:
+    """This function converts the string response from the model to a dictionary object.
+
+    Args:
+        response (str): response from the model
+
+    Returns:
+        dict: dictionary with response data
+    """
+    return json.loads(response)
+
+
+def write_response(response_dict: dict):
+    """
+    Write a response from an agent to a Streamlit app.
+
+    Args:
+        response_dict: The response from the agent.
+
+    Returns:
+        None.
+    """
+
+    # Check if the response is an answer.
+    if "answer" in response_dict:
+        st.write(response_dict["answer"])
+
+    # Check if the response is a bar chart.
+    if "bar" in response_dict:
+        data = response_dict["bar"]
+        df = pd.DataFrame(data)
+        df.set_index("columns", inplace=True)
+        st.bar_chart(df)
+
+    # Check if the response is a line chart.
+    if "line" in response_dict:
+        data = response_dict["line"]
+        df = pd.DataFrame(data)
+        df.set_index("columns", inplace=True)
+        st.line_chart(df)
+
+    # Check if the response is a table.
+    if "table" in response_dict:
+        data = response_dict["table"]
+        df = pd.DataFrame(data["data"], columns=data["columns"])
+        st.table(df)
+
+import base64
+
+LOGO_IMAGE = "insight (1).png"
+
+st.markdown(
+    """
+    <style>
+    .container {
+        display: flex;
+    }
+    .logo-text {
+        font-weight:700 !important;
+        font-size:50px !important;
+        color: #f9a01b !important;
+        padding-top: 80px !important;
+        margin-left:10px;
+    }
+    .logo-img {
+        float:right;
+        margin-top:90px;
+        height:55px;
+        width:55px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.markdown(
+    f"""
+    <div class="container">
+        <img class="logo-img" src="data:image/png;base64,{base64.b64encode(open(LOGO_IMAGE, "rb").read()).decode()}">
+        <p class="logo-text">Insights 360</p>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+# st.title("👨‍💻 Chat with your CSV")
+
+st.write("Please upload your CSV file below.")
+
+data = st.file_uploader("Upload a CSV")
+
+query = st.text_area("Insert your query")
+if data:
+    with NamedTemporaryFile() as f: # Create temporary file
+        f.write(data.getvalue())    
+
+if st.button("Submit Query", type="primary"):
+    # Create an agent from the CSV file.
+    agent = create_agent(data)
+
+    # Query the agent.
+    response = query_agent(agent=agent, query=query)
+
+    # Decode the response.
+    decoded_response = decode_response(response)
+    # decoded_response = decoded_response.decode('utf-8')
+
+    # Write the response to the Streamlit app.
+    write_response(decoded_response)
